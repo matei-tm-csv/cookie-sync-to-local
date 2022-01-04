d8ebccc0062fcc7fdafc116b480487fdf0e0e887
Mircea Matei <mircea.matei@ntgfreight.com>
2022-01-04 18:45:46 +0200

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # cookie-sync-to-local
+
+# Quick sync a cookie value to localhost
+
+Chrome extension for quick sync a cookie value from the current url to localhost
+
+# Usage
+
+## Edit popup.js
+
+Before using it setup the tracked cookie name.  
+In popup.js edit cookieName variable and change its value for the desired target.
+
+```javascript
+const cookieName =  "MyCustomCookie";
+```
+
+## Load to Chrome
+
+Go to [chrome extensions](chrome://extensions/)
+Load the extension using "Load unpacked"
+
+
+## Filtering the source url
+
+If you need to restrict the list of candidate urls place the filtered values in manifest.json. 
+Edit the file and set the "host_permissions" property
---
+++ chrome/popup.js
@@ -0,0 +1,96 @@
+const cookieName =  "MyCustomCookie";
+const successMessage = `${cookieName} was propagated.`;
+
+const form = document.getElementById("control-row");
+const go = document.getElementById("go");
+const input = document.getElementById("input");
+const message = document.getElementById("message");
+const inputlabel = document.getElementById("inputlabel");
+
+(async function initPopupWindow() {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  inputlabel.textContent = `${cookieName} cookie source URL:`;
+
+  if (tab?.url) {
+    try {
+      let url = new URL(tab.url);
+      input.value = url.origin;
+    } catch { }
+  }
+
+  input.focus();
+})();
+
+form.addEventListener("submit", handleFormSubmit);
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  clearMessage();
+
+  let url = stringToUrl(input.value);
+  if (!url) {
+    setMessage("Invalid URL");
+    return;
+  }
+
+  let message = await copyDomainCookie(url.origin);
+
+  if (message === successMessage) {
+    setMessage(message, "black");
+  }
+  else {
+    setMessage(message, "red");
+  }
+}
+
+function stringToUrl(input) {
+  try {
+    return new URL(input);
+  } catch { }
+  try {
+    return new URL("http://" + input);
+  } catch { }
+
+  return null;
+}
+
+async function copyDomainCookie(url) {
+  try {
+    const trackedCookie = await chrome.cookies.get({ url: url, name: cookieName });
+
+    if (trackedCookie.length === 0) {
+      return `No ${cookieName} cookie found`;
+    }
+
+    copyCookieToLocalhost(trackedCookie);
+  } catch (error) {
+    return `Unexpected error: ${error.message}`;
+  }
+
+  return successMessage;
+}
+
+function copyCookieToLocalhost(cookie) {
+  chrome.cookies.set({
+    url: "http://localhost",
+    name: cookie.name,
+    value: cookie.value
+  });
+
+  return cookie.name;
+}
+
+
+function setMessage(str, color) {
+  message.textContent = str;
+  message.style.color = color
+  message.hidden = false;
+}
+
+function clearMessage() {
+  message.hidden = true;
+  message.textContent = "";
+}
+
